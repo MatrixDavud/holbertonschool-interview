aa8cda216afa4d6d31b404a7a49c5915cebb4d1d
Davud <11396@gmail.com>
2025-08-04 17:37:35 +0400

    0th task

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Problem 0."""
+
+
+def canUnlockAll(boxes):
+    """Boxes is a list of lists.
+
+    each box contains keys to other boxes, or it may have no key.
+    If we can unlock all the boxes return True, False otherwise.
+    """
+    n = len(boxes)
+    answer = True
+    flag = 0
+    for i in range(1, n):
+        for j in range(n):
+            if i in boxes[j] and j != i:
+                flag = 1
+                break
+        if flag == 0:
+            answer = False
+    return answer
